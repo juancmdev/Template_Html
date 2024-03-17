785c5e9b237feffad843130588080940df9439b9
DraxusTDEV <draxustienda@gmail.com>
2024-03-16 22:44:49 -0500

    incluidas instrucciones de como hacer el template

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 #Práctica Template
 
-En este proyecto se pretende trabajar con la etiqueta ```<template>`de html el cual nos facilita el trabajo y así evitamos tener que utilizar el`innerHTML`y tener que crear cada elemento de forma independiente con`createElement````.
+#### En este proyecto se pretende trabajar con la etiqueta <template> de html el cual nos facilita el trabajo y así evitamos tener que utilizar el innerHTML y tener que crear cada elemento de forma independiente con`createElement.
 
-### Este el el [link](https://github.com/DraxusTDEV/Template_Html) del proyecro en Github por si le quieres echar un vistazo y regalarme un star no sin antes darle las gracias a [bluuweb](https://bluuweb.github.io/) por medio del cual me guié para realizar este proyecto.
+El elemento HTML <template> es un mecanismo para mantener el contenido HTML del lado del cliente que no se renderiza cuando se carga una página, pero que posteriormente puede ser instanciado durante el tiempo de ejecución empleando JavaScript.
+[@bluuweb](https://bluuweb.github.io/javascript/02-dom/#template-html)
+
+#### Este el el [link](https://github.com/DraxusTDEV/Template_Html) del proyecto en Github por si le quieres echar un vistazo y regalarme un star no sin antes darle las gracias a [@bluuweb](https://bluuweb.github.io/) por medio del cual me guié para realizar este proyecto.
--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const lista = document.querySelector("#lista");
 
-const arrayLista = ["item1", "item2", "item3", "item4"];
+const arrayLista = [
+  { nombre: "Producto 1", id: 1, precio: 250000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 2", id: 2, precio: 550000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 3", id: 3, precio: 780000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 1", id: 4, precio: 250000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 2", id: 5, precio: 550000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 3", id: 6, precio: 780000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 1", id: 7, precio: 250000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 2", id: 8, precio: 550000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 3", id: 9, precio: 780000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 3", id: 6, precio: 780000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 1", id: 7, precio: 250000, imagen: "Aquí va la imagen" },
+  { nombre: "Producto 2", id: 8, precio: 550000, imagen: "Aquí va la imagen" },
+];
+
+const template = document.querySelector("#template-li").content;
+const fragment = document.createDocumentFragment();
+
+arrayLista.forEach((item) => {
+  console.log(item);
+  template.querySelector(".list .nombre").textContent = item.nombre;
+  template.querySelector(".list #id").textContent = item.id;
+  template.querySelector(".list #precio").textContent = item.precio;
+  template.querySelector(".list #imagen").textContent = item.imagen;
+  const clone = template.cloneNode(true);
+  fragment.appendChild(clone);
+});
+
+lista.appendChild(fragment);
 
 //Forma de insertar elementos con createElement
 // const fragment = document.createDocumentFragment(); //Lo creamos para evitar el reflow
